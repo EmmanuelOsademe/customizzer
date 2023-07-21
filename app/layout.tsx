a1bd25ae9030f722f0ec5437c80fcc57f4d1981c
Emmanuel Osademe <emma.osademe@gmail.com>
2023-07-21 18:44:19 +0100

    initialising client side

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import CanvasModel from "@/canvas";
 import "./globals.css";
 
 export const metadata = {
-    title: "Custommizer",
+    title: "Customize-It",
     description: "Create and customise your designs leveraging advanced AI tools"
 }
 
@@ -10,6 +10,9 @@ const RootLayout: React.FC<{children: React.ReactNode}> = ({children}) => {
 
     return (
         <html>
+            <head>
+                <link rel="icon" type="image/x-icon" href="/emmysfav.png" />
+            </head>
             <body>
                 <main className="app transition-all ease-in">
                     {children}
